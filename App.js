dbd84687b67577a53dd36cfb5ab5396c1bcfff4a
victor rakotondranoro <adrianoviciiiz@gmail.com>
2023-06-27 17:16:39 -0500

    linked to firebase with read and write examples

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { firebaseConfig } from './firebaseConfig'; 
+import firebase from 'firebase/app';
+import { getDatabase, ref, set, child, get } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
+
+
+function writeUserData(name) {
+  const db = getDatabase();
+  set(ref(db, 'name'), {
+    username: name,
+  });
+}
+
+function readUserData(){
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `name/username`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
+
   return (
     <View style={styles.container}>
       <Text>Welcome to ISBC!</Text>
+      <TouchableOpacity style={styles.button} onPress={writeUserData("test")}>
+        <Text>Write</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={readUserData}>
+        <Text>Read</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +55,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    color: 'white',
+    backgroundColor: 'red'
+  }
 });
