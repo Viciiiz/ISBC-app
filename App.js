0a63048c2ffbdaf0fd3d746adb2422928a05d4d3
victor rakotondranoro <adrianoviciiiz@gmail.com>
2023-06-27 20:30:48 -0500

    add home screen template. Bug needs to be fixed Error: property 'App' doesn't exist

--- App.js
+++ App.js
@@ -7,8 +7,326 @@ import firebase from 'firebase/app';
 import { getDatabase, ref, set, child, get } from "firebase/database";
 import { initializeApp } from 'firebase/app';
 
+import Home from './Screens/Home';
+
 
 const app = initializeApp(firebaseConfig);
+
+// export default function App() {
+//   return(
+//       // <Home/>
+//       <View>hello</View>
+//   ) 
+// }
+
+
+// const Tab = createBottomTabNavigator();
+
+// class MyTabs extends React.Component {
+
+
+//   constructor(props) {
+//     super(props)
+//     this.handleStatusChange=this.handleStatusChange.bind(this)
+//   }
+
+//   handleStatusChange = () =>{
+//     this.props.onStatusChange()
+//   }
+
+//   render = () =>{
+//       return (
+//         <Tab.Navigator
+//           initialRouteName="Home"
+//           screenOptions={{
+//             "tabBarActiveTintColor": "#fff",
+//             "tabBarInactiveTintColor": "lightgray",
+//             "tabBarActiveBackgroundColor": "black",
+//             "tabBarInactiveBackgroundColor": "black",
+//             "tabBarStyle": [
+//               {
+//                 "display": "flex"
+//               },
+//               null
+//             ]
+//         }}>
+//                 <Tab.Screen
+//                 name="Home"
+//                 component={HomeScreen}
+//                 options={{
+//                   tabBarIcon: ({ focused }) => {
+//                     return (
+//                       <View>
+//                         <Image
+//                           source={homeImage}
+//                           resizeMode="contain"
+//                           style={{ width: 25 }}
+//                         />
+//                       </View>
+//                     );
+//                   },
+//                 }}
+//               />
+//               <Tab.Screen
+//             name="Add"
+//             component={AddScreen}
+//             options={{
+//               tabBarIcon: ({ focused }) => {
+//                 return (
+//                   <View>
+//                     <Image
+//                       source={addImage}
+//                       resizeMode="contain"
+//                       style={{ width: 25 }}
+//                     />
+//                   </View>
+//                 );
+//               },
+//             }}
+//             />
+//               <Tab.Screen
+//               name="Favorites"
+//               component={FavoritesScreen}
+//               options={{
+//                 tabBarIcon: ({ focused }) => {
+//                   return (
+//                     <View>
+//                       <Image
+//                         source={favoriteImage}
+//                         resizeMode="contain"
+//                         style={{ width: 25 }}
+//                       />
+//                     </View>
+//                   );
+//                 },
+//               }}
+//             />
+//             <Tab.Screen
+//             name="Search"
+//             component={SearchScreen}
+//             options={{
+//               tabBarIcon: ({ focused }) => {
+//                 return (
+//                   <View>
+//                     <Image
+//                       source={searchImage}
+//                       resizeMode="contain"
+//                       style={{ width: 25 }}
+//                     />
+//                   </View>
+//                 );
+//               },
+//             }}
+//             />
+//             <Tab.Screen
+//             name="Profile"
+//             children={()=><ProfileScreen onStatusChange={this.handleStatusChange}/>}
+//             options={{
+//               tabBarIcon: ({ focused }) => {
+//                 return (
+//                   <View>
+//                     <Image
+//                       source={profileImage}
+//                       resizeMode="contain"
+//                       style={{ width: 25 }}
+//                     />
+//                   </View>
+//                 );
+//               },
+//             }}
+            
+//             /> 
+//     </Tab.Navigator>
+//       );
+//   }
+// }
+
+// class Mytabslogin extends React.Component {
+
+
+//   constructor(props) {
+//     super(props)
+//     this.handleStatusChange=this.handleStatusChange.bind(this)
+//   }
+
+//   handleStatusChange = () =>{
+//     this.props.onStatusChange()
+//   }
+
+//   render = () =>{
+//       return (
+//         <Tab.Navigator
+//           initialRouteName="Profile"
+//           screenOptions={{
+//             "tabBarActiveTintColor": "#fff",
+//             "tabBarInactiveTintColor": "lightgray",
+//             "tabBarActiveBackgroundColor": "black",
+//             "tabBarInactiveBackgroundColor": "black",
+//             "tabBarStyle": [
+//               {
+//                 "display": "flex"
+//               },
+//               null
+//             ]
+//         }}>
+//                 <Tab.Screen
+//                 name="Home"
+//                 component={HomeScreen}
+//                 options={{
+//                   tabBarIcon: ({ focused }) => {
+//                     return (
+//                       <View>
+//                         <Image
+//                           source={homeImage}
+//                           resizeMode="contain"
+//                           style={{ width: 25 }}
+//                         />
+//                       </View>
+//                     );
+//                   },
+//                 }}
+//               />
+//               <Tab.Screen
+//             name="Add"
+//             component={AddScreen}
+//             options={{
+//               tabBarIcon: ({ focused }) => {
+//                 return (
+//                   <View>
+//                     <Image
+//                       source={addImage}
+//                       resizeMode="contain"
+//                       style={{ width: 25 }}
+//                     />
+//                   </View>
+//                 );
+//               },
+//             }}
+//             />
+//               <Tab.Screen
+//               name="Favorites"
+//               component={FavoritesScreen}
+//               options={{
+//                 tabBarIcon: ({ focused }) => {
+//                   return (
+//                     <View>
+//                       <Image
+//                         source={favoriteImage}
+//                         resizeMode="contain"
+//                         style={{ width: 25 }}
+//                       />
+//                     </View>
+//                   );
+//                 },
+//               }}
+//             />
+//             <Tab.Screen
+//             name="Search"
+//             component={SearchScreen}
+//             options={{
+//               tabBarIcon: ({ focused }) => {
+//                 return (
+//                   <View>
+//                     <Image
+//                       source={searchImage}
+//                       resizeMode="contain"
+//                       style={{ width: 25 }}
+//                     />
+//                   </View>
+//                 );
+//               },
+//             }}
+//             />
+//             <Tab.Screen
+//             name="Profile"
+//             children={()=><LoginScreen onStatusChange={this.handleStatusChange}/>}
+//             options={{
+//               tabBarIcon: ({ focused }) => {
+//                 return (
+//                   <View>
+//                     <Image
+//                       source={profileImage}
+//                       resizeMode="contain"
+//                       style={{ width: 25 }}
+//                     />
+//                   </View>
+//                 );
+//               },
+//             }}
+            
+//             /> 
+//     </Tab.Navigator>
+//       );
+//   }
+// }
+
+// class App extends Component {
+//   constructor(props) {
+//       super(props)
+//       this.changeLoggingState = this.changeLoggingState.bind(this)
+//       this.state = ({
+//         loggingStatus: data.isLoggedIn
+//       })
+//     }
+
+//   changeLoggingState = () =>{
+//     this.state.loggingStatus ? this.setState({loggingStatus: false}) : this.setState({loggingStatus: true})
+//   }
+  
+// 	render = () => {
+// 		return (
+//       <NavigationContainer> 
+//         {
+//           this.state.loggingStatus ? (
+//             <MyTabs onStatusChange={this.changeLoggingState}/>
+//         ) : (
+//           <LoginScreen onStatusChange={this.changeLoggingState}/>
+//             )
+//         }
+// 			</NavigationContainer>
+//     );
+//    }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+// 		flex: 1,
+// 		justifyContent: 'flex-start',
+// 		backgroundColor: 'lightgrey',
+// 	},
+//     heading: {
+// 		//flex: 0,
+// 		alignItems: 'center',
+// 		margin: 5,
+// 		padding: 100,
+// 		backgroundColor: '#1D519C',
+//   },
+  
+//   buttonBox: {
+//     flex: 1,
+//     paddingTop: 80,
+//     flexDirection: 'row',
+//     width: 150,
+//     alignItems: 'center',
+//     justifyContent: 'flex-start',
+//     },
+//   button: {
+//   paddingTop: 50,
+//   width: 80,
+//   height: 20,
+//   },
+//   headingText: {
+//     color: '#FFFFFF',
+//     fontSize: 40,
+//   },
+// });
+
+// export default App;
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
 
 export default function App() {
 
---
+++ Screens/Home.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>My App</Text>
+      </View>
+
+      <View style={styles.section}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Button 1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Button 2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Button 3</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Section Title</Text>
+        <Text style={styles.sectionText}>Section content goes here...</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 16,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'purple',
+  },
+  section: {
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'purple',
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'purple',
+    marginBottom: 10,
+  },
+  sectionText: {
+    fontSize: 16,
+    color: 'black',
+  },
+});
+
+export default App;
