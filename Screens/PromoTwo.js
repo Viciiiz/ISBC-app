bc6ad692ce9e0283b5976194519a6ea4652da80a
victor rakotondranoro <adrianoviciiiz@gmail.com>
2023-06-29 09:49:09 -0500

    added scrollable image to welcome screen

--- Screens/PromoTwo.js
+++ Screens/PromoTwo.js
@@ -1,7 +1,8 @@
 // Import necessary dependencies
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
 const PromoTwo = () => {
   // Define the clickable boxes data
@@ -49,16 +50,50 @@ const PromoTwo = () => {
     console.log(`Pressed: ${text}`);
   };
 
+  const bannerData = [
+    { image: require('../assets/banner.png') },
+    { image: require('../assets/banner2.png') },
+    { image: require('../assets/banner3.png') },
+  ];
+  const [activeSlide, setActiveSlide] = React.useState(0);
+
+  const renderBannerItem = ({ item }) => {
+    return (
+      <Image
+        source={item.image}
+        style={styles.bannerImage}
+        // resizeMode="cover"
+        resizeMode="contain"
+      />
+    );
+  };
+
   return (
     <ScrollView style={styles.container}>
-      <Image
+      {/* <Image
         source={require('../assets/logo.png')}
         style={styles.logo}
         resizeMode="contain"
+      /> */}
+      <Carousel
+        data={bannerData}
+        renderItem={renderBannerItem}
+        sliderWidth={Dimensions.get('window').width}
+        itemWidth={Dimensions.get('window').width}
+        onSnapToItem={(index) => setActiveSlide(index)}
+      />
+      <Pagination
+        dotsLength={bannerData.length}
+        activeDotIndex={activeSlide}
+        containerStyle={styles.paginationContainer}
+        dotStyle={styles.paginationDot}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
       />
       {/* <Text style={styles.title}>His Glory 2023</Text> */}
-
-      {renderBoxes()}
+      <ScrollView style={styles.containerButtons}>
+        {renderBoxes()}
+      </ScrollView>
     </ScrollView>
   );
 };
@@ -68,6 +103,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
+    // padding: 16,
+  },
+  containerButtons: {
+    // flex: 1,
+    // backgroundColor: '#ffffff',
     padding: 16,
   },
   title: {
@@ -83,6 +123,25 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     // marginBottom: 16,
     // marginBottom: -8,
+  },
+  paginationContainer: {
+    paddingVertical: 10,
+    // paddingHorizontal: 5
+  },
+  paginationDot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 8,
+    backgroundColor: 'rgba(0, 0, 0, 0.92)',
+  },
+  bannerImage: {
+    // width: '100%',
+    // height: 200,
+    // marginRight: 10,
+    width: '100%',
+    height: undefined,
+    aspectRatio: 16 / 9,
   },
   box: {
     width: '50%',
@@ -102,7 +161,8 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: '100%',
-    height: 120,
+    height: 100,
+    marginBottom: 10
   },
 });
 
